84587b234a276140be4ba3c3f4871e1e1fc70204
geighz <38474544+geighz@users.noreply.github.com>
2021-01-08 21:23:48 -0600

    Backend code for logging in and registering.

---
+++ api/src/Registration.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
+import DropdownList from 'react-widgets/lib/DropdownList'
+import 'react-widgets/dist/css/react-widgets.css';
+
+const types = [ { type: 'Single Volunteer or Volunteer Group', value: '1' },
+{ type: 'Hub', value: '2' } ]
+
+
+class Registration extends React.Component {
+
+  renderError({error, touched}){
+    if (touched && error){
+      return(
+        <div className = 'ui error message'>
+          <div className = "header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) =>{
+    const className = `field ${meta.error && meta.touched ? 'error': ''}`;
+    return(
+      <div className = {className}>
+      <label>{label}</label>
+      <input {...input} autoComplete = "off" />
+      {this.renderError(meta)}
+      </div>
+    );
+  }
+  renderDropdownList = ({ input, data, valueField, textField, label }) =>{
+  return(
+    <div className = 'field'>
+      <label>{label}</label>
+      <DropdownList {...input}
+        label = {label}
+        data= {data}
+        valueField={valueField}
+        textField={textField}
+        onChange={input.onChange} />
+      </div>
+    );
+  }
+
+  onSubmit(formValues){
+    console.log(formValues);
+  }
+
+constructor(props) {
+    super(props)
+    this.state = {
+        username: '',
+        email: '',
+        type: '',
+        password: '',
+        passwordCheck: ''
+    }
+}
+
+  render(){
+  return (
+    <div className="ui container">
+      <h1 className = "ui center aligned huge header">
+        Glad to have you on board! Time to Register.
+        <div className="sub header">Fill out the information below.</div>
+      </h1>
+<form action="register.php" onSubmit={this.handleSubmit.bind(this)} method="POST">
+      <Field name="username" component={this.renderInput} label="Create Username"/>
+      <Field name="email" component={this.renderInput} label = "Email"/>
+      <Field
+          name="type"
+          component={this.renderDropdownList}
+          data={types}
+          valueField="value"
+          textField="type"
+          label = "Type of Member"/>
+      <Field name="password" component={this.renderInput} label = "Create Password"/>
+      <Field name="passwordCheck" component={this.renderInput} label = "Confirm Password"/>
+      <button className="ui button primary">Submit</button>
+      </form>
+    </div>
+  );
+  }
+};
+
+const validate = (formValues) =>{
+  const error = {};
+  if (!formValues.username) {
+    // only ran if the user did not enter a Username.
+    error.username = 'Username field is missing.';
+  }
+  if(!formValues.password){
+    error.password = "Password field is missing.";
+  }
+  if(!formValues.email){
+    error.email = "Email field is missing.";
+  }
+  if(formValues.password != formValues.passwordCheck){
+    error.passwordCheck = "Passwords do not match.";
+  }
+
+  return error;
+};
+
+export default reduxForm({
+  form: 'Registration',
+  validate: validate
+})(Registration);
---
+++ api/src/users.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 20, 2020 at 06:08 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wordpress`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1=Active | 0=Inactive',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `phone`, `created`, `modified`, `status`) VALUES
+(1, 'David', 'Dearborn', 'dave@dear.cc', '172522ec1028ab781d9dfd17eaca4427', '', '2020-11-30 00:42:14', '2020-11-30 00:42:14', 1),
+(2, 'Sean', 'McMany', 'smacmany@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', '', '2020-11-30 02:04:18', '2020-11-30 02:04:18', 1);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
