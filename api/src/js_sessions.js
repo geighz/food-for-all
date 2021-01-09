98c02ddc09f90ccef040fff6556d8c7c43306a1e
Tower Mutasa <53870487+towercodes@users.noreply.github.com>
2021-01-09 05:22:26 +0200

    Correct Login api files

--- api/src/js_sessions.js
+++
@@ -1,1 +0,0 @@
-var test = "Does this work?";
--- api/src/pseudologin.php
+++ api/src/pseudologin.php
@@ -4,7 +4,7 @@ Form: <br>
 <form action = "login.php" method = "post">
 Username: <br> <input type = "text" name = "username"><br>
 Password: <br> <input type = "text" name = "password"><br>
-<input type  = "submit" value = "register">
+<input type  = "submit" value = "login">
 </form>
 </body>
 </html>
